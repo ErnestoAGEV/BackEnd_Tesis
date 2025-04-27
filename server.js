be7901e89c9ba4b7388a2b135acd9c8550fd58e1
ErnestoAGEV <vevelerne@gmail.com>
2025-04-27 14:13:23 -0600

    Primer commit , inicio del servidor y configuraciones

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require ('express');
+const app = express();
+const http = require ('http');
+const server = http.createServer(app);
+const logger = require ('morgan');
+const cors = require('cors');
+
+
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+
+app.disable('x-powered-by');
+
+app.set('port', port);
+
+server.listen(3000, '192.168.1.80' || 'localhost', function() {
+    console.log('Apliacion de NodeJS ' + port + ' Iniciada...')
+});
+
+app.get('/', (req, res) => {
+    res.send('Ruta raiz del backend');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Esta es la ruta test');
+});
+
+
+//Error handling
+app.use((error,req,res,next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
+
+
+// 200 respuesta exitosa
+// 404 significa que no se ha encontrado la ruta
+// 500 error interno del servidor
